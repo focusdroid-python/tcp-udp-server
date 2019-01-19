80869863b9f8815974b73d2ca5d43404bf58fc41
focusdroid-python <focusdroid@126.com>
2019-01-19 19:24:47 +0800

    服务器返回静态资源

---
+++ 2019/01/0119/sockets.py
@@ -0,0 +1,35 @@
+import socket
+
+def service_client(new_socket):
+    rquest = new_socket.recv(1024)
+    # print(request)
+
+    response = "HTTP/1.1 200 OK \r\n"
+    response += "\r\n"
+
+    response += "<body><h1>Hello Python</h1></body>"
+    new_socket.send(response.encode("utf-8"))
+
+    new_socket.close()
+
+
+def main():
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    tcp_server_socket.bind(('', 7891))
+
+    tcp_server_socket.listen(128)
+
+
+    while True:
+        new_socket, client_addr = tcp_server_socket.accept()
+
+        service_client(new_socket)
+
+    tcp_server_socket.close()
+
+
+
+
+if __name__  ==  '__main__':
+    main()
